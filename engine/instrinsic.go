a59f3a52536da7ce73427f2bd4e57b8fe391c584
lsytj0413 <511121939@qq.com>
2022-11-18 12:22:24 +0800

    feat(swf): remove asl support
    Signed-off-by: lsytj0413 <511121939@qq.com>

--- engine/instrinsic.go
+++
@@ -1,8 +0,0 @@
-package engine
-
-const (
-	FuncFormat       string = "States.Format"
-	FuncStringToJson string = "States.StringToJson"
-	FuncJsonToString string = "States.JsonToString"
-	FuncArray        string = "States.Array"
-)
